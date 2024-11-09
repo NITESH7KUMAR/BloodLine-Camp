58ac2f1c079111995c5852876767a77bbcfb746f
NITESH KUMAR <119055912+NITESH7KUMAR@users.noreply.github.com>
2024-11-09 23:41:35 +0530

    footer adding

--- src/component/Donatenow/DonateNow.js
+++ src/component/Donatenow/DonateNow.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Footer from '../Footer/Footer';
 function DonateNow() {
   return (
     <div>
         <h2>DonateNow</h2>
+        <Footer/>
     </div>
   )
 }
